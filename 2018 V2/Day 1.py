7ffdeca0734910d3e1a4cc033e93d76ac50bd9a6
Ishaan Patel <ipatel9@stevens.edu>
2019-05-20 02:06:22 -0400

    Optimzed Code

--- 2018 V2/Day 1.py
+++ 2018 V2/Day 1.py
@@ -7,23 +7,26 @@ Advent Of Code 2018 - Day 1 V2
 FILE = 'day1_input.txt'
 lst = open(FILE, 'r').readlines()
 
-'''Part 1, returns the sum of all values'''
-val = 0
-for x in lst:
-    val += int(x)
-print(val)
-
-'''Part 2, returns the first sum to be seen twice'''
-val = 0
-sums = {}
-result = 0
-done = False
-while not done:
+def sum():
+    """Part 1, returns the sum of all values"""
+    val = 0
     for x in lst:
-        if val in sums:
-            done = True
-            result = val
-        else:
-            sums[val] = 1
-            val += int(x)
-print(result)
+        val += int(x)
+    return val
+
+def second():
+    """Part 2, returns the first sum to be seen twice"""
+    val = 0
+    sums = {}
+    done = False
+
+    while not done:
+        for x in lst:
+            if val in sums:
+                return val
+            else:
+                sums[val] = 1
+                val += int(x)
+
+print(sum())
+print(second())
